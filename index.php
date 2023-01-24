60230527b8704efd1a32862eddad8cab31ad2b4c
John Memmott <122105474+RATIU5@users.noreply.github.com>
2023-01-24 14:14:38 -0700

    updated the path to search files

--- index.php
+++ index.php
@@ -167,7 +167,7 @@
 
                         <ul class="ul-tree">
                             <?php
-                            define("WD", "/var/www/localhost/htdocs/www");
+                            define("WD", "/htdocs/www");
 
                             function listFolderFiles($dir)
                             {
