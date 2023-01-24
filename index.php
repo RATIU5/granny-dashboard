bea314422deba7e39d40c6f79b84cc2546098ead
John Memmott <122105474+RATIU5@users.noreply.github.com>
2023-01-24 15:34:06 -0700

    refactor: refactored tabs

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -299,7 +298,6 @@
 			<p>Copyright &copy; Granny Software & Co</p>
 		</footer>
 	</div>
-
 </body>
 
 </html>
